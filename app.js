a21ab49e450e8b7f194a26106556540fa701d57b
fareehaabbasi <abbasifareeha9@gmail.com>
2025-06-02 14:44:44 -0700

    complete

---
+++ app.js
@@ -0,0 +1,18 @@
+const textArea = document.querySelector("#textarea");
+const btn = document.querySelector("#btn");
+const output = document.querySelector(".output");
+
+btn.addEventListener("click", function() {
+    if(textArea === "") {
+        output.innerHTML = "Please enter a some text"
+    }else{
+        const text = textArea.value;
+        let count = 0;
+        for (var i = 0; i < text.length; i++){
+            if(text[i] === "a" || text[i] === "e" || text[i] === "i" || text[i] === "o" || text[i] === "u"){
+                count++
+            }
+        }
+        output.innerHTML = `The numbers of Vowels are ${count}`;
+    }
+});
